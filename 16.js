b0076a503a7c1c274c9ef7466b5c8df8b76ae480
Emanuele <emanuele.t.soares@aluno.senai.br>
2026-09-15 08:00:48 -0300

    Modify user2 object and comments in 16.js
    Updated user2 object name and comments.

--- 16.js
+++ 16.js
@@ -1,7 +1,7 @@
 //Crie um objeto com método arrow sayThis() e analise como this se comporta. Compare com função tradicional.
 
 const user2 = {
-    name: "Giovanna",
+    name: "Emanuele",
 
     sayThis: function () {
         console.log(this);
